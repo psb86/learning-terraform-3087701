e1c52bee67a10074461cf381bd9884b1cbfa1f49
psb86 <127174837+psb86@users.noreply.github.com>
2023-05-01 15:44:29 +0530

    module sg

--- main.tf
+++ main.tf
@@ -17,7 +17,8 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+ # vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
     Name = "HelloWorld"
   }
@@ -60,4 +61,14 @@ resource "aws_security_group_rule" "blog_everything_out" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+  vpc_id  = data.aws_vpc.default.id
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
